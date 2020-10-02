f2b4a3db49934c41379048c83f0e224adf6d9755
User <sebastian.mariani.94@gmail.com>
2020-10-02 16:57:38 +0100

    basic server functions

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello express!')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('about page!')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('weather page')
+})
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000.')
+})
