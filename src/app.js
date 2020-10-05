2073358a401668dbc8bc900bf57df585a34d479c
User <sebastian.mariani.94@gmail.com>
2020-10-05 17:39:32 +0100

    adding handlebars for dynamic values

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Frank'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Frank'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        error: 'An error message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
