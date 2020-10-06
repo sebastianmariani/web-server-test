79cf153a31db4389d885d1b22fb10a61fd6f1465
User <sebastian.mariani.94@gmail.com>
2020-10-06 17:20:46 +0100

    added advance template

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for express configuration
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -30,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req,res) => {
     res.render('help', {
-        error: 'An error'
+        error: 'An error',
+        title: 'Help',
+        name: 'Frank'
     })
 })
 
