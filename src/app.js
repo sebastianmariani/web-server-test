b408d3c4177c7e441353a284bb0f2289d9ea7338
User <sebastian.mariani.94@gmail.com>
2020-11-18 09:56:32 +0000

    sending json back

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -40,10 +42,37 @@ app.get('/help', (req,res) => {
 })
 
 app.get('/weather', (req, res) => {
+    const address = req.query.address
+    if (!address) {
+        return res.send({
+            error:'You must provide an address'
+        })
+    }
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                location,
+                forecast: forecastData,
+                address,
+                })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
     res.send({
-        location: 'philadelphia',
-        longitude: 30.404,
-        latitude: 10.455
+        products: []
     })
 })
 
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7180aac380a211a6f59853956ef3fa0d&query=' + latitude + ',' + longitude + ''
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback(`Unable to find location.`, undefined)
+        } else {
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like there are ${body.current.feelslike} degrees.`)
+        }
+    }) 
+}
+
+module.exports = forecast
